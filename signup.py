cb929002472e5afad18b96638e1d66e52c0e09c5
Sam Clements <sam@borntyping.co.uk>
2012-09-28 18:35:54 +0100

    Editing, cleaning and email list

--- signup.py
+++ signup.py
@@ -7,6 +7,7 @@ from flask import Flask, flash, render_template, request, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.wtf import Form, TextField, BooleanField, ValidationError
 
+from sqlalchemy import orm
 from sqlalchemy.exc import IntegrityError
 from wtforms.ext.sqlalchemy.orm import model_form
 from wtforms.validators import Required, Email
@@ -23,17 +24,15 @@ class Member (db.Model):
 	id	  = db.Column(db.Integer, primary_key=True)
 	name  = db.Column(db.String(80), unique=True) #: The members name
 	email = db.Column(db.String(120), unique=True) #: The members email address
-	bos   = db.Column(db.String(10), unique=True) #: The members BOS number
+	bos   = db.Column(db.String(10)) #: The members BOS number
 	paid  = db.Column(db.Boolean) #: Has the member paid the membership fee?
 	
 	def __init__ (self, name, email, bos, paid):
 		self.__dict__.update(locals())
-		if self.bos == "":
-			self.bos = None
-		
+
 	def __repr__ (self):
 		return "<%s '%s'>" % (self.__class__.__name__, self.name)
-	
+
 class SignupForm (Form):	
 	name = TextField("Name", [Required()])
 	
@@ -55,6 +54,12 @@ class SignupForm (Form):
 	
 	paid = BooleanField("Paid", description="The membership fee is &pound;3 - pay now?")
 
+class EditForm (Form):	
+	name = TextField("Name", [Required()])
+	email = TextField("Email", [Required(), Email()], default="@aber.ac.uk")
+	bos = TextField("BOS Number")
+	paid = BooleanField("Paid", description="Has the member paid?")
+
 @app.route("/", methods=['GET', 'POST'])
 def signup ():
 	form = SignupForm(request.form)
@@ -75,10 +80,27 @@ def list ():
 	count = len(members)
 	return render_template("list.html", **locals())
 
+@app.route("/emails")
+def emails ():
+	members = Member.query.all()
+	count = len(members)
+	return ", ".join([member.email for member in members])
+
+@app.route("/clean")
+def clean ():
+	members = Member.query.all()
+	for member in members:
+		member.name = member.name.title()
+		if member.bos == "":
+			member.bos = None
+		db.session.add(member)
+	db.session.commit()
+	return "Database cleaned"
+
 @app.route("/edit/<id>", methods=['GET', 'POST'])
 def edit (id):
 	member = Member.query.filter_by(id=id).first()
-	form = SignupForm(request.form, obj=member)
+	form = EditForm(request.form, obj=member)
 	if form.validate_on_submit():
 		form.populate_obj(member)
 		db.session.add(member)
